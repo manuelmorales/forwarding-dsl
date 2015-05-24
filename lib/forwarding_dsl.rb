289d96ff4a74fab6e445d3e1f67361df56762553
Manuel Morales <manuelmorales@gmail.com>
2015-05-24 22:20:45 +0100

    Added run to the parent module

--- lib/forwarding_dsl.rb
+++ lib/forwarding_dsl.rb
@@ -2,4 +2,8 @@ require "forwarding_dsl/version"
 
 module ForwardingDsl
   autoload :Dsl, 'forwarding_dsl/dsl'
+
+  def self.run *args, &block
+    Dsl.run *args, &block
+  end
 end
--- spec/forwarding_dsl_spec.rb
+++ spec/forwarding_dsl_spec.rb
@@ -4,4 +4,13 @@ describe ForwardingDsl do
   it 'has a version' do
     expect(ForwardingDsl::VERSION).not_to be_nil
   end
+
+  it 'delegates .run() to Dsl' do
+    target = double('target')
+    expect(target).to receive(:some_method)
+
+    ForwardingDsl.run target do
+      some_method
+    end
+  end
 end
