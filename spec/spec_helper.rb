9e005c904dabe68ad05fc8c18c066d626864b147
Manuel Morales <manuelmorales@gmail.com>
2015-07-06 18:42:58 +0100

    Test Coverage badge

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,15 @@
 require 'rubygems'
 require 'rspec'
 require 'pry'
+require 'codeclimate-test-reporter'
 
 RSpec.configure do |config|
    config.color = true
    config.tty = true
    config.formatter = :documentation # :documentation, :progress, :html, :textmate
 end
+
+CodeClimate::TestReporter.start
 
 $LOAD_PATH.unshift File.expand_path('lib')
 require 'forwarding_dsl'
